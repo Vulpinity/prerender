f1dc3ea3a62c442d29925c35cd5c5974d54d2629
Fox Piacenti <fox@vulpinity.com>
2018-11-01 18:58:19 +0000

    Try another cacher.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var prerender = require('./lib');
 var server = prerender();
 
 console.log(process.env)
-server.use(require('prerender-file-cache'));
+server.use(require('prerender-filesystem-cache'));
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
